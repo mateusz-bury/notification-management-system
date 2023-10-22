23604799b0f6961d0fd6ef281974dd713d1e9fc1
mateuszbury420 <mateuszbury875@gmail.com>
2023-10-22 20:55:05 +0200

    contoler_cz_2

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System_zarządzania_błędami.Data;
+using System_zarządzania_błędami.Models;
 
 namespace System_zarządzania_błędami.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
         }
     }
 }
