9ec6227ae11bf14e70eb51d7a63ad1a0968c3e0e
Mateusz Bury <m.bury@predictivesolutions.pl>
2023-10-30 11:13:32 +0100

    form

--- Models/Dane.cs
+++ Models/Dane.cs
@@ -2,25 +2,11 @@
 
 namespace System_zarządzania_błędami.Models
 {
-    public class Dane
+    public class ContactFormModel
     {
-        public string Imie { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
-        public string Temat { get; set; }
-        public string Tresc { get; set; }
-    }
-
-    public class WalidacjaDanych: Dane
-    {
-        [Required(ErrorMessage = "Prosze podaj Imie")]
-        public string Imie { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Email")]
-        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Temat")]
-        public string Temat { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Tresc wiadomosci")]
-        [MinLength(10), MaxLength(50)]
-        public string Tresc { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
     }
 }
